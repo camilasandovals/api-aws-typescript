f5a5a6295b1decb693cf157236eef77c8cafd8db
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-04 16:10:08 -0400

    added dogs

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ import cors from "cors"
 
 const app = express ()
 
+const dogs = [
+    {
+    name: "Paco",
+    age: 34,
+    breed: "Chihuaha"
+    },
+    {
+        name: "Dora",
+        age: 5,
+        breed: "Yorki"
+    },
+    {
+        name: "Pony",
+        age: 15,
+        breed: "Labrador"
+    }
+]
+
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req,res) => res.send('Welcome to my API'))
+app.get('/', (req,res) => res.json(dogs))
 
 app.listen(4000, () => console.log("API running 😃"))
